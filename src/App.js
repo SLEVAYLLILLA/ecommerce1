d5beafb5ef3bcbd07c54c662cbf045f6ec64cb8a
Lilla Somfaine Levay <s.levay.lilla@gmail.com>
2023-08-28 07:40:18 +0200

    ecommerce

--- src/App.js
+++ src/App.js
@@ -2,48 +2,7 @@ import React, { useState, useEffect } from "react";
 import Amazon from "./components/amazon";
 import Navbar from "./components/navbar";
 import Cart from "./components/cart";
-// import { list } from './data.js';
-
-//const Gallery = () => {
-  //const [index, setIndex] = useState(0);
-  //const [showMore, setShowMore] = useState(false);
-  //const hasNext = index < list.length - 1;
- 
-  //function handleNextClick() {
-  //  if (hasNext) {
-   //   setIndex(index + 1);
-    //} else {
-    //  setIndex(0);
-    //}
-  //}
-
-
-  function handleMoreClick() {
-    setShowMore(!showMore);
-  }
-
-  let sculpture = list[index];
-  return (
-    <>
-      <h2>
-        <i>{sculpture.title} </i>
-        by {sculpture.author}
-      </h2>
-      <h3>
-        ({index + 1} of {list.length})
-      </h3>
-      <button onClick={handleMoreClick}>
-        {showMore ? 'Hide' : 'Show'} details
-      </button>
-      {showMore && <p>{sculpture.description}</p>}
-      <img
-        src={sculpture.url}
-        alt={sculpture.alt}
-      />
-    </>
-  );
-
-
+import './styles/amazon.css';
 
 
 const App = () => {
@@ -64,7 +23,7 @@ const App = () => {
     setCart([...arr]);
   };
 
-  
+
   return (
     <React.Fragment>
       <Navbar setShow={setShow} size={cart.length} />
